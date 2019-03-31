f0bf6e3b6d43bbbe0d5537757f304b98b181ce5a
MatsnevV <v.matsnev@yandex.ru>
2019-03-31 23:10:14 +0300

    добавление пользователя

--- webapp/config.py
+++ webapp/config.py
@@ -6,3 +6,5 @@ WEATHER_DEFAULT_CITY = "Moscow,Russia"
 WEATHER_API_KEY = "a9b09d28f6334a54b0d104854191203"
 WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+
+SECRET_KEY = "afasfasf42336**%!#$Jqegqegqezxcxbbnm)(&(^__)+@#$@532225247890-=09H325>CKAOEPfdsgH:"
--- webapp/model.py
+++ webapp/model.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 db = SQLAlchemy()
 
 class News(db.Model):
@@ -11,3 +14,25 @@ class News(db.Model):
 
     def __repr__(self):
         return f'<News {self.title} {self.url}>'
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+
+
+    def __repr__(self):
+        return f'<User {self.username} id={self.id}>'
+
