82875654df1237309d765a32b91cac8e4ebb6f8e
MatsnevV <v.matsnev@yandex.ru>
2019-05-05 14:55:09 +0300

    резерв

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -1,11 +1,13 @@
-from flask_login import current_user, login_required
+
 from flask import Blueprint
+
+from webapp.user.decorators import admin_required
+
+
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
 @blueprint.route('/')
-@login_required
+@admin_required
 def admin_index():
-    if current_user.is_admin:
-        return 'Привет админ'
-    else:
-        return 'Ты не админ!'
+    return 'Привет админ'
+
